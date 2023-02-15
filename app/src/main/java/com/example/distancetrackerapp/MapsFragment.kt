089b89cbd9d924a54a9972a1b69d2a0c95e64f6e
Chanadech Jaroenkunathum <ChanadechJ@varuna.co.th>
2023-02-16 06:23:34 +0700

    Add onMapReady and add UI using data-binding

--- app/src/main/java/com/example/distancetrackerapp/MapsFragment.kt
+++ app/src/main/java/com/example/distancetrackerapp/MapsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.distancetrackerapp.databinding.FragmentMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,26 +15,42 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(),OnMapReadyCallback {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View{
+       _binding = FragmentMapsBinding.inflate(inflater, container, false)
+
+        binding.startButton.setOnClickListener{
+
+        }
+        binding.stopButton.setOnClickListener{
+
+        }
+        binding.refreshButton.setOnClickListener {
+
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapFragment?.getMapAsync(this)      // this from map fragment which implement OnMapReadyCallback -> ทำท่า callback แบบ implement แทน
+    }
+
+    override fun onMapReady(p0: GoogleMap) {  // trigger every-time when map ready
+//        TODO("Not yet implemented")
+    }
+
+    override fun onDestroyView() {  // help to handle memory leak
+        super.onDestroyView()
+        _binding = null
     }
 }
